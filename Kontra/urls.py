91b07feb3a80d1371e4787b4dfbb901ff2543e05
Vaibhav5 Gupta <vaibhavgupta.xl@gmail.com>
2024-07-11 13:42:25 +0530

    login

---
+++ Kontra/urls.py
@@ -0,0 +1,31 @@
+"""Kontra URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from accounts.views import AccountEmailVerificationSentView, AccountEmailConfirmationView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/registration/account-email-verification-sent/', AccountEmailVerificationSentView.as_view(), name='account_email_verification_sent'),
+    path('accounts/', include('accounts.urls')),
+    # path('dj-rest-auth/registration/account-email-verification-sent/', TemplateView.as_view(template_name='account/email/account_email_verification_sent.html'), name='account_email_verification_sent'),
+    path('dj-rest-auth/registration/account-confirm-email/<str:key>/', TemplateView.as_view(template_name='account/email/account_confirm_email.html'), name='account_confirm_email'),
+
+]
